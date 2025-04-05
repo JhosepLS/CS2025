ccea306884fbd3f2a38a68463b4aeb5e6bd33c93
JhosepLS <jmollapazam@ulasalle.edu.pe>
2025-04-05 02:26:13 -0500

    Actualizando las carpetas y el Readme

--- flask_oauth_project/run.py
+++
@@ -1,7 +0,0 @@
-from threading import Thread
-from app.oauth_server import oauth_server
-from app.backend_server import backend_server
-
-if __name__ == "__main__":
-    Thread(target=lambda: oauth_server.run(port=5000, debug=True, use_reloader=False)).start()
-    Thread(target=lambda: backend_server.run(port=5001, debug=True, use_reloader=False)).start()
